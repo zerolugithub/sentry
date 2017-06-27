fb61455bf0df13b9feeeba51df83995ab291bd97
Ted Kaemming <ted@kaemming.com>
2017-06-26 17:21:07 -0700

    Remove all references to serialization in feature building, switch to interface objects instead of raw data.

--- src/sentry/similarity/__init__.py
+++ src/sentry/similarity/__init__.py
@@ -7,17 +7,11 @@ from sentry.similarity.features import (
     MessageFeature,
     FeatureSet,
     get_application_chunks,
-    get_exception_frames,
-    serialize_frame,
-    serialize_text_shingle,
 )
 from sentry.similarity.index import MinHashIndex
 from sentry.utils import redis
 from sentry.utils.datastructures import BidirectionalMapping
 from sentry.utils.iterators import shingle
-
-
-FRAME_SEPARATOR = b'\x02'
 
 
 features = FeatureSet(
@@ -43,44 +37,24 @@ features = FeatureSet(
     }),
     {
         'exception:message:character-shingles': ExceptionFeature(
-            lambda exception: map(
-                serialize_text_shingle,
-                shingle(
-                    13,
-                    exception.get('value') or '',
-                ),
-            )
-        ),
-        'exception:stacktrace:application-chunks': ExceptionFeature(
-            lambda exception: map(
-                lambda frames: FRAME_SEPARATOR.join(
-                    map(
-                        serialize_frame,
-                        frames,
-                    ),
-                ),
-                get_application_chunks(exception),
+            lambda exception: shingle(
+                13,
+                exception.value,
             ),
         ),
+        'exception:stacktrace:application-chunks': ExceptionFeature(
+            lambda exception: get_application_chunks(exception),
+        ),
         'exception:stacktrace:pairs': ExceptionFeature(
-            lambda exception: map(
-                FRAME_SEPARATOR.join,
-                shingle(
-                    2,
-                    map(
-                        serialize_frame,
-                        get_exception_frames(exception),
-                    ),
-                ),
+            lambda exception: shingle(
+                2,
+                exception.stacktrace.frames,
             ),
         ),
         'message:message:character-shingles': MessageFeature(
-            lambda message: map(
-                serialize_text_shingle,
-                shingle(
-                    13,
-                    message['message'],
-                ),
+            lambda message: shingle(
+                13,
+                message.message,
             ),
         ),
     }
