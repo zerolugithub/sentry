48ec546f8f60febaa5399744a8c839c8eece88dc
Jess MacQueen <jess@getsentry.com>
2017-08-03 18:27:26 -0700

    make sure github apps won't be shown in repo dropdown, correct api param on frontend

--- src/sentry/api/endpoints/organization_config_repositories.py
+++ src/sentry/api/endpoints/organization_config_repositories.py
@@ -8,15 +8,25 @@ from sentry import features
 
 
 class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
+    def has_access(self, organization, provider_id):
+        if provider_id == 'bitbucket' and not features.has(
+            'organizations:bitbucket-repos', organization
+        ):
+            return False
+
+        # TODO(jess): figure out better way to exclude this
+        if provider_id == 'github_apps':
+            return False
+
+        return True
+
     def get(self, request, organization):
         provider_bindings = bindings.get('repository.provider')
 
         providers = []
         for provider_id in provider_bindings:
             provider = provider_bindings.get(provider_id)(id=provider_id)
-            if provider_id != 'bitbucket' or features.has(
-                'organizations:bitbucket-repos', organization
-            ):
+            if self.has_access(organization, provider_id):
                 providers.append(
                     {
                         'id': provider_id,
