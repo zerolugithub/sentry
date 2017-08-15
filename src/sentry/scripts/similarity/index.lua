bebeff9649e2263ff23a26d6f6ba7bb70f320ccf
Ted Kaemming <ted@kaemming.com>
2017-08-15 14:40:55 -0700

    Rewrite argument parser

--- src/sentry/scripts/similarity/index.lua
+++ src/sentry/scripts/similarity/index.lua
@@ -23,26 +23,48 @@ This is modeled as two data structures:
 
 ]]--
 
+
 -- Try and enable script effects replication if we're using Redis 3.2 or
 -- greater. This is wrapped in `pcall` so that we can continue to support older
 -- Redis versions while using this feature if it's available.
---[[
 if not pcall(redis.replicate_commands) then
     redis.log(redis.LOG_DEBUG, 'Could not enable script effects replication.')
 end
-]]--
 
---[[
-local reduce = function (callback, initializer, iterator, state, ...)
-    local v = initializer
-    local i = {iterator(state, ...)}
-    while i[1] ~= nil do
-        v = callback(v, unpack(i))
-        i = {iterator(state, i[1])}
-    end
-    return v
+
+-- Utilities
+
+local function identity(...)
+    return ...
 end
-]]--
+
+local function partial(target, ...)
+    local bound_arguments = {...}
+
+    return function (...)
+        local call_arguments = {}
+
+        for i = 1, #bound_arguments do
+            call_arguments[i] = bound_arguments[i]
+        end
+
+        for j, argument in ipairs({...}) do
+            call_arguments[i + j] = argument
+        end
+
+        return target(unpack(call_arguments))
+    end
+end
+
+local function default_table(default)
+    return setmetatable({}, {
+        __index = function (table, key)
+            local value = default(key)
+            table[key] = value
+            return value
+        end,
+    })
+end
 
 local function redis_hash_response_iterator(response)
     local i = 1
@@ -53,17 +75,8 @@ local function redis_hash_response_iterator(response)
     end
 end
 
-function table.slice(t, start, stop)
-    if stop == nil then
-        stop = #t
-    end
 
-    local result = {}
-    for i = start, stop do
-        result[i - start + 1] = t[i]
-    end
-    return result
-end
+-- Distance Calculation
 
 local function get_manhattan_distance(target, other)
     local keys = {}
@@ -98,211 +111,261 @@ local function scale_to_total(values)
     return result
 end
 
-local function parse_configuration(arguments)
-    local configuration = {
-        scope = arguments[1],
-        bands = tonumber(arguments[2]),
-        window = tonumber(arguments[3]),
-        retention = tonumber(arguments[4]),
-        timestamp = tonumber(arguments[5]),
-        byte_order = '>',  -- network byte order
-        band_format = 'B',
-        bucket_format = 'H',
-    }
 
-    local TimeSeriesSet = {}
+-- Time Series Set
 
-    function TimeSeriesSet:new(key_function)
-        return setmetatable({
-            key_function = key_function,
-        }, {
-            __index = self,
-        })
+local TimeSeriesSet = {}
+
+function TimeSeriesSet:new(window, retention, key_function)
+    return setmetatable({
+        window = window,
+        retention = retention,
+        key_function = key_function,
+    }, {__index = self})
+end
+
+function TimeSeriesSet:insert(timestamp, ...)
+    local index = math.floor(timestamp / self.window)
+    local key = self.key_function(index)
+    local result = redis.call('SADD', key, ...)
+    if result > 0 then
+        -- The expiration only needs updating if we've actually added anything.
+        redis.call('EXPIREAT', key, (index + 1 + self.retention) * self.window)
     end
+    return result
+end
 
-    -- TODO: Some of this time series stuff should probably be extracted.
-
-    function TimeSeriesSet:insert(...)
-        local index = math.floor(configuration.timestamp / configuration.window)
-        local key = self.key_function(index)
-        local result = redis.call('SADD', key, ...)
-        if result > 0 then
-            -- The expiration only needs updating if we've actually added anything.
-            redis.call('EXPIREAT', key, (index + 1 + configuration.retention) * configuration.window)
+function TimeSeriesSet:members(timestamp)
+    local results = {}
+    local current = math.floor(timestamp / self.window)
+    for index = current - self.retention, current do
+        local members = redis.call('SMEMBERS', self.key_function(index))
+        for i = 1, #members do
+            local k = members[i]
+            results[k] = (results[k] or 0) + 1
         end
-        return result
     end
+    return results
+end
 
-    function TimeSeriesSet:members()
-        local results = {}
-        local current = math.floor(configuration.timestamp / configuration.window)
-        for index = current - configuration.retention, current do
-            local members = redis.call('SMEMBERS', self.key_function(index))
-            for i = 1, #members do
-                local k = members[i]
-                results[k] = (results[k] or 0) + 1
+
+-- Configuration
+
+local Configuration = {}
+
+function Configuration:new(scope, bands, window, retention, timestamp)
+    return setmetatable({
+        scope = scope,
+        bands = bands,
+        window = window,
+        retention = retention,
+        timestamp = timestamp,
+        frequency_key_format = '>BH',
+    }, {__index = self})
+end
+
+function Configuration:pack_frequency_key(band, bucket)
+    return struct.pack(self.frequency_key_format, band, bucket)
+end
+
+function Configuration:unpack_frequency_key(key)
+    return struct.unpack(self.frequency_key_format, key)
+end
+
+function Configuration:get_candidate_index(index, band, bucket)
+    local key_prefix = table.concat({
+        self.scope,
+        index,
+        self:pack_frequency_key(band, bucket),
+        '',
+    }, ':')
+    return TimeSeriesSet:new(
+        self.window,
+        self.retention,
+        function (i)
+            return key_prefix .. i
+        end
+    )
+end
+
+function Configuration:get_candidates(index, frequencies)
+    local candidates = default_table(
+        function ()
+            local value = {}
+            for band = 1, self.bands do
+                value[band] = 0
             end
+            return value
         end
-        return results
-    end
-
-    configuration.frequency_key_format = string.format(
-        '%s%s%s',
-        configuration.byte_order,
-        configuration.band_format,
-        configuration.bucket_format
     )
 
-    function configuration:pack_frequency_key(band, bucket)
-        return struct.pack(self.frequency_key_format, band, bucket)
-    end
-
-    function configuration:unpack_frequency_key(key)
-        return struct.unpack(self.frequency_key_format, key)
-    end
-
-    function configuration:get_candidate_index(index, band, bucket)
-        return TimeSeriesSet:new(
-            function (i)
-                return string.format('%s:c:%s:', self.scope, index) .. self:pack_frequency_key(bucket, band) .. string.format(':%s', i)
-            end
-        )
-    end
-
-    function configuration:get_frequencies(index, item)
-        local frequencies = {}
-        for band = 1, self.bands do
-            frequencies[band] = {}
-        end
-
-        local response = redis.call('HGETALL', string.format('%s:f:%s:%s', self.scope, index, item))
-        for key, count in redis_hash_response_iterator(response) do
-            local band, bucket = self:unpack_frequency_key(key)
-            frequencies[band][bucket] = tonumber(count)
-        end
-
-        return frequencies
-    end
-
-    function configuration:set_frequencies(index, item, frequencies)
-        local arguments = {}
-        local key = string.format('%s:f:%s:%s', self.scope, index, item)
-        for band = 1, self.bands do
-            for bucket, count in pairs(frequencies[band]) do
-                redis.call('HINCRBY', key, self:pack_frequency_key(band, bucket), count)
+    for band = 1, self.bands do
+        for bucket, _ in pairs(frequencies[band]) do
+            for candidate, _ in pairs(self:get_candidate_index(index, band, bucket):members(configuration.timestamp)) do
+                candidates[candidate][band] = candidates[candidate][band] + 1
             end
         end
-        redis.call('EXPIREAT', key, self.timestamp + (self.retention * self.window))
     end
-
-    function configuration:get_candidates(index, frequencies)
-        local candidates = setmetatable({}, {
-            __index = function (t, k)
-                t[k] = {}
-                return t[k]
-            end,
-        })
-
-        for band = 1, self.bands do
-            for bucket, _ in pairs(frequencies[band]) do
-                for candidate, _ in pairs(self:get_candidate_index(index, band, bucket):members()) do
-                    local hits = candidates[candidate]
-                    hits[band] = (hits[band] or 0) + 1
-                end
-            end
-        end
-
-        local results = {}
-        for candidate, bands in pairs(candidates) do
-            local collisions = 0
-            for band, buckets in pairs(bands) do
-                collisions = collisions + 1
-            end
-            results[candidate] = collisions
-        end
-        return results
-    end
-
-    function configuration:calculate_similarity(target, other)
-        local total = 0
-        for band = 1, self.bands do
-            local distance = get_manhattan_distance(
-                scale_to_total(target[band]),
-                scale_to_total(other[band])
-            )
-            total = total + (1 - distance / 2)
-        end
-        return total / self.bands
-    end
-
-    return configuration, arguments[6], table.slice(arguments, 7)
-end
-
-local parse_repeated_arguments = function (parser, arguments, cursor)
-    local count = tonumber(arguments[cursor])
-    cursor = cursor + 1
 
     local results = {}
-    for i = 1, count do
-        cursor, results[i] = parser(arguments, cursor)
+    for candidate, bands in pairs(candidates) do
+        local collisions = 0
+        for band = 1, self.bands do
+            if bands[band] > 0 then
+                collisions = collisions + 1
+            end
+        end
+        results[candidate] = collisions
     end
-
-    return cursor, results
+    return results
 end
 
-local parse_frequencies = function (configuration, arguments, cursor)
+function Configuration:get_frequencies(index, item)
     local frequencies = {}
-    for i = 1, configuration.bands do
-        local buckets = {}
-        cursor = parse_repeated_arguments(
-            function (arguments, cursor)
-                buckets[tonumber(arguments[cursor])] = tonumber(arguments[cursor + 1])
-                return cursor + 2
-            end,
-            arguments,
-            cursor
-        )
-        frequencies[i] = buckets
+    for band = 1, self.bands do
+        frequencies[band] = {}
     end
-    return cursor, frequencies
+
+    local response = redis.call('HGETALL', string.format('%s:f:%s:%s', self.scope, index, item))
+    for key, count in redis_hash_response_iterator(response) do
+        local band, bucket = self:unpack_frequency_key(key)
+        frequencies[band][bucket] = tonumber(count)
+    end
+
+    return frequencies
 end
+
+function Configuration:set_frequencies(index, item, frequencies)
+    local key = string.format('%s:f:%s:%s', self.scope, index, item)
+    for band = 1, self.bands do
+        for bucket, count in pairs(frequencies[band]) do
+            redis.call('HINCRBY', key, self:pack_frequency_key(band, bucket), count)
+        end
+    end
+    redis.call('EXPIREAT', key, self.timestamp + (self.retention * self.window))
+end
+
+function Configuration:calculate_similarity(target_frequencies, other_frequencies)
+    local sum = 0
+    for band = 1, self.bands do
+        local distance = get_manhattan_distance(
+            scale_to_total(target_frequencies[band]),
+            scale_to_total(other_frequencies[band])
+        )
+        sum = sum + (1 - distance / 2)
+    end
+    return sum / self.bands
+end
+
+
+-- Argument Parsing
+
+local function argument_parser(callback)
+    if callback == nil then
+        callback = identity
+    end
+
+    return function (cursor, arguments)
+        return cursor + 1, callback(arguments[cursor])
+    end
+end
+
+local function repeated_argument_parser(argument_parser, quantity_parser)
+    if quantity_parser == nil then
+        quantity_parser = function (cursor, arguments)
+            return cursor + 1, tonumber(arguments[cursor])
+        end
+    end
+
+    return function (cursor, arguments)
+        local results = {}
+        local cursor, count = quantity_parser(cursor, arguments)
+        for i = 1, count do
+            cursor, results[i] = argument_parser(cursor, arguments)
+        end
+        return cursor, results
+    end
+end
+
+local function object_argument_parser(schema, callback)
+    if callback == nil then
+        callback = identity
+    end
+
+    return function (cursor, arguments)
+        local result = {}
+        for i, specification in ipairs(schema) do
+            local key, parser = unpack(specification)
+            cursor, result[key] = parser(cursor, arguments)
+        end
+        return cursor, result
+    end
+end
+
+local function variadic_argument_parser(argument_parser)
+    return function (cursor, arguments)
+        local results = {}
+        local i = 1
+        while arguments[cursor] ~= nil do
+            cursor, results[i] = argument_parser(cursor, arguments)
+            i = i + 1
+        end
+        return cursor, results
+    end
+end
+
+local function frequencies_argument_parser(bands)
+    return repeated_argument_parser(
+        function (cursor, arguments)
+            local buckets = {}
+            return repeated_argument_parser(
+                function (cursor, arguments)
+                    buckets[tonumber(arguments[cursor])] = tonumber(arguments[cursor + 1])
+                    return cursor + 2
+                end
+            )(cursor, arguments), buckets
+        end,
+        function (cursor, arguments)
+            return cursor, bands
+        end
+    )
+end
+
+
+-- Command Execution
 
 local commands = {
-    RECORD = function (configuration, arguments)
-        local requests = {}
-        local cursor = 1
-        while arguments[cursor] ~= nil do
-            local request = {
-                key = arguments[cursor],
-                index = arguments[cursor + 1],
-            }
-            cursor, request.frequencies = parse_frequencies(configuration, arguments, cursor + 2)
-            requests[#requests + 1] = request
-        end
+    RECORD = function (configuration, cursor, arguments)
+        local cursor, requests = variadic_argument_parser(
+            object_argument_parser({
+                {"key", argument_parser()},
+                {"index", argument_parser()},
+                {"frequencies", frequencies_argument_parser(configuration.bands)},
+            })
+        )(cursor, arguments)
 
-        for i, request in ipairs(requests) do
+        redis.breakpoint()
+        error('not implemented')
+
+        for request in requests do
             configuration:set_frequencies(request.index, request.key, request.frequencies)
             for band = 1, configuration.bands do
                 for bucket, _ in pairs(request.frequencies[band]) do
-                    configuration:get_candidate_index(request.index, band, bucket):insert(request.key)
+                    configuration:get_candidate_index(request.index, band, bucket):insert(configuration.timestamp, request.key)
                 end
             end
         end
     end,
-    COMPARE = function (configuration, arguments)
+    CLASSIFY = function (configuration, cursor, arguments)
+        local cursor, requests = variadic_argument_parser(
+            object_argument_parser({
+                {"index", argument_parser()},
+                {"threshold", argument_parser(tonumber)},
+                {"frequencies", frequencies_argument_parser(configuration.bands)},
+            })
+        )(cursor, arguments)
+
         error('not implemented')
-    end,
-    CLASSIFY = function (configuration, arguments)
-        local requests = {}
-        local cursor = 1
-        while arguments[cursor] ~= nil do
-            local request = {
-                index = arguments[cursor],
-                threshold = tonumber(arguments[cursor]),
-            }
-            cursor, request.frequencies = parse_frequencies(configuration, arguments, cursor + 2)
-            requests[#requests + 1] = request
-        end
 
         local candidates = setmetatable({}, {
             __index = function (t, k)
@@ -334,13 +397,29 @@ local commands = {
 
         return results
     end,
-    MERGE = function (configuration, arguments)
-        error('not implemented')
-    end,
-    DELETE = function (configuration, arguments)
-        error('not implemented')
-    end,
 }
 
-local configuration, command, arguments = parse_configuration(ARGV)
-return commands[command](configuration, arguments)
+local cursor, configuration = object_argument_parser({
+    {"scope", argument_parser()},
+    {"bands", argument_parser(tonumber)},
+    {"window", argument_parser(tonumber)},
+    {"retention", argument_parser(tonumber)},
+    {"timestamp", argument_parser(tonumber)},
+}, function (obj)
+    return Configuration:new(
+        obj.scope,
+        obj.bands,
+        obj.window,
+        obj.retention,
+        obj.timestamp
+    )
+end)(1, ARGV)
+
+local command
+cursor, command = argument_parser(
+    function (argument)
+        return commands[argument]
+    end
+)(cursor, ARGV)
+
+return command(configuration, cursor, ARGV)
