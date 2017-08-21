bfa5e53ef7ba79aa1ef1c4c3eda070a1dc8e6760
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-08-21 16:21:35 -0700

    Correct pagination prev cursor for empty sets

--- src/sentry/utils/cursors.py
+++ src/sentry/utils/cursors.py
@@ -85,7 +85,7 @@ class CursorResult(Sequence):
         )
 
 
-def build_cursor(results, key, limit=100, cursor=None, hits=None, max_hits=None):
+def build_cursor(results, key, limit=100, is_desc=False, cursor=None, hits=None, max_hits=None):
     if cursor is None:
         cursor = Cursor(0, 0, 0)
 
@@ -108,7 +108,7 @@ def build_cursor(results, key, limit=100, cursor=None, hits=None, max_hits=None)
     # Default cursor if not present
     if is_prev:
         next_value = value
-        next_offset = offset
+        next_offset = 0
         has_next = True
     elif num_results:
         if not value:
@@ -121,14 +121,31 @@ def build_cursor(results, key, limit=100, cursor=None, hits=None, max_hits=None)
         next_value = int(key(results[-1]))
 
         if next_value == value:
+            # value has not changed, page forward by adjusting the offset
             next_offset = offset + limit
         else:
+            # We have an absolute value to page from. If any of the items in
+            # the current result set come *after* or *before* (depending on the
+            # is_desc flag) we will want to increment the offset to account for
+            # moving past them.
+            #
+            # This is required to account for loss of precision in the key value.
             next_offset = 0
             result_iter = reversed(results)
-            # skip the last result
-            six.next(result_iter)
+
+            # If we have more results the last item in the results should be
+            # skipped, as we know we want to start from that item and do not
+            # need to offset from it.
+            if has_next:
+                six.next(result_iter)
+
             for result in result_iter:
-                if int(key(result)) == next_value:
+                result_value = int(key(result))
+
+                is_larger = result_value >= next_value
+                is_smaller = result_value <= next_value
+
+                if (is_desc and is_smaller) or (not is_desc and is_larger):
                     next_offset += 1
                 else:
                     break
@@ -137,31 +154,62 @@ def build_cursor(results, key, limit=100, cursor=None, hits=None, max_hits=None)
         next_offset = offset
         has_next = False
 
-    # Determine what our pervious cursor is by ensuring we have a unique offset
+    # If the cursor contains previous results, the first item is the item that
+    # indicates if we have more items later, and is *not* the first item im the
+    # list, that should be used for the value.
+    first_prev_index = 1 if is_prev and has_prev else 0
+
+    # If we're paging back we need to calculate the key from the first result
+    # with for_prev=True to ensure rounding of the key is correct.See
+    # sentry.api.paginator.BasePaginator.get_item_key
+    prev_value = int(key(results[first_prev_index], for_prev=True)) if results else 0
+
+    # Determine what our previous cursor is by ensuring we have a unique offset
     if is_prev and num_results:
-        prev_value = int(key(results[0]))
-
-        if num_results > 2:
-            i = 1
-            while i < num_results and prev_value == int(key(results[i])):
-                i += 1
-            i -= 1
-        else:
-            i = 0
-
-        # if we iterated every result and the offset didn't change, we need
-        # to simply add the current offset to our total results (visible)
+        # If we don't have an earlier value just add the offset
         if prev_value == value:
-            prev_offset = offset + i
+            prev_offset = offset + limit
         else:
-            prev_offset = i
-    else:
-        # previous cursor is easy if we're paginating forward
-        prev_value = value
-        prev_offset = offset
+            # Just as above, we may need to add an offset if any of the results at
+            # the beginning are *before* or *after* (depending on the is_desc
+            # flag).
+            #
+            # This is required to account for loss of precision in the key value.
+            prev_offset = 0
+            result_iter = iter(results)
 
-    # Truncate the list to our original result size now that we've determined the next page
-    results = results[:limit]
+            # If we know there are more previous results, we need to move past
+            # the item indicating that more items exist.
+            if has_prev:
+                six.next(result_iter)
+
+            # Always move past the first item, this is the prev_value item and will
+            # already be offset in the next query.
+            six.next(result_iter)
+
+            for result in result_iter:
+                result_value = int(key(result, for_prev=True))
+
+                is_larger = result_value >= prev_value
+                is_smaller = result_value <= prev_value
+
+                # Note that the checks are reversed here as a prev query has
+                # it's ordering reversed.
+                if (is_desc and is_larger) or (not is_desc and is_smaller):
+                    prev_offset += 1
+                else:
+                    break
+    else:
+        # Prev only has an offset if the cursor we were dealing with was a
+        # previous cursor. Otherwise we'd be taking the offset while moving forward.
+        prev_offset = offset if is_prev else 0
+
+    # Truncate the list to our original result size now that we've determined
+    # the next/prev page
+    if is_prev and has_prev:
+        results = results[1:]
+    elif not is_prev:
+        results = results[:limit]
 
     next_cursor = Cursor(next_value or 0, next_offset, False, has_next)
     prev_cursor = Cursor(prev_value or 0, prev_offset, True, has_prev)
